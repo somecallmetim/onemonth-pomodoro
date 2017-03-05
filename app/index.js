2ac1e2656aff28ca0e75194ff7627044bdacf935
somecallmetim <timbauer@ymail.com>
2017-03-05 15:51:35 -0800

    added instructional message and ability to choose how long timer should run

--- app/index.js
+++ app/index.js
@@ -3,79 +3,97 @@ class PomodoroTimer extends React.Component{
     constructor(){
         super();
         self = this;
-        this.state = {timeLeft: '--:--', timeElapsed: 0};
+        this.state = {timeLeft: 'Select a time and press start! When time expires, take 5!',
+            timeElapsed: 0, workInterval: 25, isClockRunning: false};
     }
 
-    totalTime(workInterval, restInterval){
-        return (workInterval + restInterval);
+    changeTimerLength(e){
+        self.setState({workInterval: e.target.value});
     }
-
-    // componentDidMount(){
-    //     this.interval = setInterval(this.elapsedTime.bind(this),1000);
-    //     this.setState({start: new Date()});
-    // }
 
     startTimer(){
-        self.interval = setInterval(self.elapsedTime.bind(self),1000);
-        self.setState({start: new Date()});
+        if(self.state.isClockRunning === false){
+            var workIntervalSeconds = self.state.workInterval*60;
+            self.interval = setInterval(self.elapsedTime.bind(self),1000);
+            self.setState({isClockRunning: true});
+            self.setState({timeLeft: self.composeTime(workIntervalSeconds)});
+            self.setState({start: new Date()});
+            self.setState({workIntervalSeconds: workIntervalSeconds})
+        }
     }
 
     resetTimer(){
-        clearInterval(self.interval);
-        self.setState({timeLeft: '--:--'});
+        if(self.state.isClockRunning === true){
+            clearInterval(self.interval);
+            self.setState({isClockRunning: false});
+            self.setState({timeLeft: self.props.instructionalGreeting});
+        }
     }
 
-    componentWillUnmount(){
-        clearInterval(this.interval);
-        this.setState({timeLeft: '--:--'});
-    }
-
-    elapsedTime(){
-        var currentTime = new Date();
-        var timeElapsed = Math.floor((new Date() - this.state.start)/1000);
-        this.setState({timeElapsed: timeElapsed});
-
-        var workIntervalSeconds = this.props.workInterval * 60;
-        var timeLeft = workIntervalSeconds - timeElapsed;
-
-        var minutes = Math.floor(timeLeft / 60);
-        var seconds = timeLeft - minutes * 60;
+    composeTime(timeInSeconds){
+        var minutes = Math.floor(timeInSeconds / 60);
+        var seconds = timeInSeconds - minutes * 60;
         if(seconds < 10){
             seconds = "0" + seconds;
         }
         var minutesSeconds = minutes + ":" + seconds;
 
+        return minutesSeconds;
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
+        this.setState({timeLeft: this.props.instructionalGreeting});
+    }
+
+    elapsedTime(){
+        var timeElapsed = Math.floor((new Date() - this.state.start)/1000);
+        this.setState({timeElapsed: timeElapsed});
+
+        var timeLeft = this.state.workIntervalSeconds - timeElapsed;
+
+        var minutesSeconds = this.composeTime(timeLeft);
+
         this.setState({timeLeft: minutesSeconds});
 
-        if(this.state.timeElapsed >= (this.props.workInterval)*60){
+        if(this.state.timeElapsed >= (this.state.workIntervalSeconds)){
             clearInterval(this.interval);
             alert('Time to rest!');
-            this.setState({timeLeft: '--:--'});
+            this.setState({timeLeft: this.props.instructionalGreeting});
+            this.setState({isClockRunning: false});
         };
     }
 
     render(){
         return (
-            // <div>
-            //     This timer funs for {this.props.workInterval} minutes, followed by a rest
-            //     of {this.props.restInterval} minutes, <br/>for a total time
-            //     of {this.totalTime(this.props.workInterval, this.props.restInterval)} minutes.
-            //     <br/>There are {this.state.timeElapsed} seconds elapsed.
-            // </div>
 
             <div className="container">
 
                 <div className="row first-row">
                     <div className="col-xs-8 col-sm-6 col-md-4 col-centered">
-                        <div className="panel panel-primary">
+                        <div className="panel panel-primary pomodoro-panel">
                             <div className="panel-heading">
                                 <h3 className="panel-title">
                                     Welcome to your Pomodoro Timer!
                                 </h3>
                             </div>
-                            <div className="panel-body clock">
+                            <div className={this.state.isClockRunning ? "clock panel-body" : "clock-text panel-body"}>
                                 {this.state.timeLeft}
                             </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-4 col-sm-3 col-med-2 col-centered">
+                        <div className="container fill-columns">
+                            <select className="select-timer-dropdown" defaultValue={this.state.workInterval} onChange={this.changeTimerLength}>
+                                <option value='0.05'>test</option>
+                                <option value='15'>15 minutes</option>
+                                <option value='20'>20 minutes</option>
+                                <option value='25'>25 minutes</option>
+                                <option value='30'>30 minutes</option>
+                            </select>
                         </div>
                     </div>
                 </div>
@@ -100,6 +118,6 @@ class PomodoroTimer extends React.Component{
 }
 
 ReactDOM.render(
-    <PomodoroTimer workInterval={25} restInterval={5} timeElapsed={'xx'} />,
+    <PomodoroTimer instructionalGreeting={'Select a time and press start! When time expires, take 5!'} />,
     document.getElementById('app')
 );
--- build/index.js
+++ build/index.js
@@ -3,122 +3,164 @@ class PomodoroTimer extends React.Component {
     constructor() {
         super();
         self = this;
-        this.state = { timeLeft: '--:--', timeElapsed: 0 };
+        this.state = { timeLeft: 'Select a time and press start! When time expires, take 5!',
+            timeElapsed: 0, workInterval: 25, isClockRunning: false };
     }
 
-    totalTime(workInterval, restInterval) {
-        return workInterval + restInterval;
+    changeTimerLength(e) {
+        self.setState({ workInterval: e.target.value });
     }
-
-    // componentDidMount(){
-    //     this.interval = setInterval(this.elapsedTime.bind(this),1000);
-    //     this.setState({start: new Date()});
-    // }
 
     startTimer() {
-        self.interval = setInterval(self.elapsedTime.bind(self), 1000);
-        self.setState({ start: new Date() });
+        if (self.state.isClockRunning === false) {
+            var workIntervalSeconds = self.state.workInterval * 60;
+            self.interval = setInterval(self.elapsedTime.bind(self), 1000);
+            self.setState({ isClockRunning: true });
+            self.setState({ timeLeft: self.composeTime(workIntervalSeconds) });
+            self.setState({ start: new Date() });
+            self.setState({ workIntervalSeconds: workIntervalSeconds });
+        }
     }
 
     resetTimer() {
-        clearInterval(self.interval);
-        self.setState({ timeLeft: '--:--' });
+        if (self.state.isClockRunning === true) {
+            clearInterval(self.interval);
+            self.setState({ isClockRunning: false });
+            self.setState({ timeLeft: self.props.instructionalGreeting });
+        }
     }
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-        this.setState({ timeLeft: '--:--' });
-    }
-
-    elapsedTime() {
-        var currentTime = new Date();
-        var timeElapsed = Math.floor((new Date() - this.state.start) / 1000);
-        this.setState({ timeElapsed: timeElapsed });
-
-        var workIntervalSeconds = this.props.workInterval * 60;
-        var timeLeft = workIntervalSeconds - timeElapsed;
-
-        var minutes = Math.floor(timeLeft / 60);
-        var seconds = timeLeft - minutes * 60;
+    composeTime(timeInSeconds) {
+        var minutes = Math.floor(timeInSeconds / 60);
+        var seconds = timeInSeconds - minutes * 60;
         if (seconds < 10) {
             seconds = "0" + seconds;
         }
         var minutesSeconds = minutes + ":" + seconds;
 
+        return minutesSeconds;
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+        this.setState({ timeLeft: this.props.instructionalGreeting });
+    }
+
+    elapsedTime() {
+        var timeElapsed = Math.floor((new Date() - this.state.start) / 1000);
+        this.setState({ timeElapsed: timeElapsed });
+
+        var timeLeft = this.state.workIntervalSeconds - timeElapsed;
+
+        var minutesSeconds = this.composeTime(timeLeft);
+
         this.setState({ timeLeft: minutesSeconds });
 
-        if (this.state.timeElapsed >= this.props.workInterval * 60) {
+        if (this.state.timeElapsed >= this.state.workIntervalSeconds) {
             clearInterval(this.interval);
             alert('Time to rest!');
-            this.setState({ timeLeft: '--:--' });
+            this.setState({ timeLeft: this.props.instructionalGreeting });
+            this.setState({ isClockRunning: false });
         };
     }
 
     render() {
-        return (
-            // <div>
-            //     This timer funs for {this.props.workInterval} minutes, followed by a rest
-            //     of {this.props.restInterval} minutes, <br/>for a total time
-            //     of {this.totalTime(this.props.workInterval, this.props.restInterval)} minutes.
-            //     <br/>There are {this.state.timeElapsed} seconds elapsed.
-            // </div>
-
+        return React.createElement(
+            "div",
+            { className: "container" },
             React.createElement(
-                'div',
-                { className: 'container' },
+                "div",
+                { className: "row first-row" },
                 React.createElement(
-                    'div',
-                    { className: 'row first-row' },
+                    "div",
+                    { className: "col-xs-8 col-sm-6 col-md-4 col-centered" },
                     React.createElement(
-                        'div',
-                        { className: 'col-xs-8 col-sm-6 col-md-4 col-centered' },
+                        "div",
+                        { className: "panel panel-primary pomodoro-panel" },
                         React.createElement(
-                            'div',
-                            { className: 'panel panel-primary' },
+                            "div",
+                            { className: "panel-heading" },
                             React.createElement(
-                                'div',
-                                { className: 'panel-heading' },
-                                React.createElement(
-                                    'h3',
-                                    { className: 'panel-title' },
-                                    'Welcome to your Pomodoro Timer!'
-                                )
-                            ),
-                            React.createElement(
-                                'div',
-                                { className: 'panel-body clock' },
-                                this.state.timeLeft
-                            )
-                        )
-                    )
-                ),
-                React.createElement(
-                    'div',
-                    { className: 'row' },
-                    React.createElement(
-                        'div',
-                        { className: 'container col-xs-8 col-sm-6 col-md-4 col-centered' },
-                        React.createElement(
-                            'div',
-                            { className: 'pull-left' },
-                            React.createElement(
-                                'button',
-                                {
-                                    onClick: this.startTimer,
-                                    className: 'btn btn-success main-button' },
-                                'Start'
+                                "h3",
+                                { className: "panel-title" },
+                                "Welcome to your Pomodoro Timer!"
                             )
                         ),
                         React.createElement(
-                            'div',
-                            { className: 'pull-right' },
+                            "div",
+                            { className: this.state.isClockRunning ? "clock panel-body" : "clock-text panel-body" },
+                            this.state.timeLeft
+                        )
+                    )
+                )
+            ),
+            React.createElement(
+                "div",
+                { className: "row" },
+                React.createElement(
+                    "div",
+                    { className: "col-xs-4 col-sm-3 col-med-2 col-centered" },
+                    React.createElement(
+                        "div",
+                        { className: "container fill-columns" },
+                        React.createElement(
+                            "select",
+                            { className: "select-timer-dropdown", defaultValue: this.state.workInterval, onChange: this.changeTimerLength },
                             React.createElement(
-                                'button',
-                                {
-                                    onClick: this.resetTimer,
-                                    className: 'btn btn-danger main-button' },
-                                'Reset'
+                                "option",
+                                { value: "0.05" },
+                                "test"
+                            ),
+                            React.createElement(
+                                "option",
+                                { value: "15" },
+                                "15 minutes"
+                            ),
+                            React.createElement(
+                                "option",
+                                { value: "20" },
+                                "20 minutes"
+                            ),
+                            React.createElement(
+                                "option",
+                                { value: "25" },
+                                "25 minutes"
+                            ),
+                            React.createElement(
+                                "option",
+                                { value: "30" },
+                                "30 minutes"
                             )
+                        )
+                    )
+                )
+            ),
+            React.createElement(
+                "div",
+                { className: "row" },
+                React.createElement(
+                    "div",
+                    { className: "container col-xs-8 col-sm-6 col-md-4 col-centered" },
+                    React.createElement(
+                        "div",
+                        { className: "pull-left" },
+                        React.createElement(
+                            "button",
+                            {
+                                onClick: this.startTimer,
+                                className: "btn btn-success main-button" },
+                            "Start"
+                        )
+                    ),
+                    React.createElement(
+                        "div",
+                        { className: "pull-right" },
+                        React.createElement(
+                            "button",
+                            {
+                                onClick: this.resetTimer,
+                                className: "btn btn-danger main-button" },
+                            "Reset"
                         )
                     )
                 )
@@ -127,4 +169,4 @@ class PomodoroTimer extends React.Component {
     }
 }
 
-ReactDOM.render(React.createElement(PomodoroTimer, { workInterval: 25, restInterval: 5, timeElapsed: 'xx' }), document.getElementById('app'));
+ReactDOM.render(React.createElement(PomodoroTimer, { instructionalGreeting: 'Select a time and press start! When time expires, take 5!' }), document.getElementById('app'));
